b813b8f66bde40cc28a594bb059fef4567365a26
Jan Warchoł <jan.warchol@gmail.com>
2020-01-07 22:19:56 +0100

    Add samples from limestone
    Commit 14a8d0e468703df4187b6b8c1f224a122a1a770b

---
+++ jan-warchol/limestone_build_theme.py
@@ -0,0 +1,93 @@
+"""Generate a theme file from palette, styling and config template."""
+
+from __future__ import division
+from jinja2 import Template
+from pprint import pprint
+from os import path
+import sys
+import palette_builder as builder
+import importlib
+import argparse
+import re
+
+
+def calc_output_path(values, input_path):
+    """Build config filename from palette info and template name."""
+    directory = path.dirname(input_path)
+    template_fname = path.basename(input_path).replace(".j2", "")
+    extension = re.sub(re.compile(r'.*template\.'), '', template_fname)
+    filename = values["slug"] + "-" + values["version"] + "." + extension
+    return path.join(directory, filename)
+
+
+def build_pycharm_snippet(color, style):
+    """Wrap styling information in format appropriate for PyCharm config."""
+    attributes = set(style.split())
+    result = ['<option name="FOREGROUND" value="{}" />'.format(color)]
+
+    if "bold" in attributes and "italic" in attributes:
+        result.append('<option name="FONT_TYPE" value="3" />')
+    elif "bold" in attributes:
+        result.append('<option name="FONT_TYPE" value="1" />')
+    elif "italic" in attributes:
+        result.append('<option name="FONT_TYPE" value="2" />')
+
+    if "underline" in attributes:
+        result.append('<option name="EFFECT_COLOR" value="{}" />'.format(color))
+        result.append('<option name="EFFECT_TYPE" value="1" />')
+
+    return '\n        '.join(result)
+
+
+def build_vscode_snippet(color, style):
+    """Wrap styling information in format appropriate for VScode config."""
+    result = [
+        '"fontStyle": "{}",'.format(style),
+        '"foreground": "#{}"'.format(color)
+    ]
+    return '\n				'.join(result)
+
+
+def build_templating_values(palette, styling_rules):
+    """Create a dict with all attributes that can be used in a template."""
+    mapping = {
+        "name": palette.name,
+        "slug": palette.slug,
+    }
+    with open("VERSION", "r") as f:
+        mapping["version"] = f.read().strip()
+
+    for token, (color_name, style) in styling_rules.items():
+        rgb_color = palette.rgb_values()[color_name]
+        mapping[token] = {
+            "color": rgb_color,
+            "style": style,
+            "pycharm": build_pycharm_snippet(rgb_color, style),
+            "vscode": build_vscode_snippet(rgb_color, style),
+        }
+
+    # we want to use color names directly e.g. for editor-specific settings
+    mapping.update(palette.rgb_values())
+
+    return mapping
+
+
+# Parse options and run templating.
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument("--palette", "-p", required=True)
+parser.add_argument("--template", "-t", required=True)
+parser.add_argument("--styling", "-s", required=True)
+args = parser.parse_args()
+
+palette = builder.Palette.load_from_path(args.palette)
+token_styling = importlib.import_module(args.styling)
+values = build_templating_values(palette, token_styling.rules)
+
+with open(args.template, "r") as f:
+    template = Template(f.read())
+
+output_path = calc_output_path(values, args.template)
+with open(output_path, "w") as f:
+    f.write(template.render(**values))
+    print("Output written to {}".format(output_path))
---
+++ jan-warchol/limestone_token_styling.py
@@ -0,0 +1,48 @@
+"""Define styling (color and formatting) for common token types.
+
+Color should be a name available in Palette.color_coords dictionary.
+Formatting should be either "normal" or a combination of
+- boid
+- italic
+- underline
+(order doesn't matter)."""
+
+rules = {
+    # non-code (information and metadata)
+    "comment":         ("dim_0", "italic"),
+    "doc_comment":     ("dim_0", "normal"),
+    "doc_special":     ("dim_1", "bold"),
+
+    # literals and constants
+    "string":          ("dim_1", "bold"),
+    "str_special":     ("fg_2",  "normal"),
+    "number":          ("fg_0",  "normal"),
+    "boolean":         ("fg_0",  "italic"),
+    "constant":        ("fg_0",  "italic"),
+
+    # language reserved words
+    "keyword":         ("fg_1",  "bold"),
+    "import":          ("fg_1",  "bold"),
+    "operator":        ("fg_0",  "normal"),
+
+    # "meta" tokens that supplement other elements
+    "type":            ("dim_1", "bold italic"),
+    "keyword_arg":     ("dim_3", "italic"),
+    "special_var":     ("dim_1", "bold italic"),
+
+    # elements that are usually predefined
+    "built_in":        ("fg_0",  "italic"),
+    "exception":       ("fg_2",  "bold"),
+    "decorator":       ("dim_3", "bold italic"),
+
+    # elements that are usually user-defined
+    "class_def":       ("fg_1",  "bold underline"),
+    "function_def":    ("fg_1",  "underline"),
+    "function_call":   ("fg_0",  "normal"),
+    # some grammars use one scope for function definitions and calls
+    "function":        ("fg_0",  "normal"),
+    "parameter":       ("fg_0",  "normal"),
+
+    # other
+    "punctuation":     ("fg_2",  "normal"),
+}
